cf67ab03407cbb561920b0d601b21b28a88fbdfc
mardymar <plutoofpaso2004@hotmail.com>
2016-03-30 17:24:38 -0700

    Added html comments

--- app/login/index.js
+++ app/login/index.js
@@ -1,4 +1,4 @@
-
+//Log out on load
 if ( Parse.User.current() ) {
     Parse.User.logOut();
     // check if really logged out
@@ -6,11 +6,13 @@ if ( Parse.User.current() ) {
         alert("Failed to log out!");
 }
 
+//Called when the sign in button is clicked
 function login(){
     event.preventDefault();
     submitToParse(document.getElementById('username').value, document.getElementById('password').value);
 }
 
+//Login using Parse
 function submitToParse(username, password) {
     Parse.User.logIn(username.toLowerCase(), password, {
         success: function (user) {
